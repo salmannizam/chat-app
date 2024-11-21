507ae699bc5e2a93a66c3fb8d77fb0a293014714
salmannizam <salmannizamraja418812@gmail.com>
2024-11-21 12:21:30 +0530

    rood id page

---
+++ chat-frontend/src/app/chat/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Chat(){
+    return (
+        <div className="chat">
+            <div className="chat-header">
+                <h2>Chat</h2>
+
+            </div>
+        </div>
+        );
+
+
+}
